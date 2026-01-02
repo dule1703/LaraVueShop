e4f587f1400dc6496217130d2086f08ddb08c331
dule1703 <duledrljaca@yahoo.com>
2026-01-02 09:26:01 +0100

    Create Order and OrderItem models

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total_price',
+        'status',
+        'customer_email',
+        'customer_name',
+        'shipping_address',
+    ];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
